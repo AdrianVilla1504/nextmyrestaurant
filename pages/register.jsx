fd9d9cae67849b96ba171c8881a1ba4684b0f6f9
adrianvilla1504 <adriancvilla@gmail.com>
2022-09-17 18:24:10 -0500

    feat: add home page,  register, login layout

---
+++ pages/register.jsx
@@ -0,0 +1,98 @@
+/* eslint-disable @next/next/no-img-element */
+
+import { LockClosedIcon } from '@heroicons/react/20/solid'
+
+const register = () => {
+  return (
+    <div className="bg-white min-h-full pt-[30px] pb-[140px]">
+      <div className="flex h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div className="w-full max-w-md space-y-8">
+          <div>
+            <img
+              className="mx-auto h-12 w-auto"
+              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+              alt="Your Company"
+            />
+            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+              Register to taste the best food in town
+            </h2>
+          </div>
+          <form className="mt-8 space-y-6" action="#" method="POST">
+            <input type="hidden" name="remember" defaultValue="true" />
+            <div className="-space-y-px rounded-md shadow-sm">
+            <div>
+                <label htmlFor="name" className="sr-only">
+                  Email address
+                </label>
+                <input
+                  id="name"
+                  name="name"
+                  type="text"
+                  autoComplete="name"
+                  required
+                  className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  placeholder="Name"
+                />
+              </div>
+              <div>
+                <label htmlFor="email-address" className="sr-only">
+                  Email address
+                </label>
+                <input
+                  id="email-address"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="relative block w-full appearance-none rounded-none border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  placeholder="Email address"
+                />
+              </div>
+              <div>
+                <label htmlFor="phone-number" className="sr-only">
+                  Phone Number
+                </label>
+                <input
+                  id="phone-number"
+                  name="phone-number"
+                  type="number"
+                  autoComplete="phone-number"
+                  required
+                  className="relative block w-full appearance-none rounded-none border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  placeholder="Phone number"
+                />
+              </div>
+              <div>
+                <label htmlFor="password" className="sr-only">
+                  Password
+                </label>
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                  placeholder="Password"
+                />
+              </div>
+            </div>
+            <div>
+              <button
+                type="submit"
+                className="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              >
+                <span className="absolute inset-y-0 left-0 flex items-center pl-3">
+                  <LockClosedIcon className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
+                </span>
+                Sign up
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default register;
---
+++ services/products.js
@@ -0,0 +1,71 @@
+const BASE_URL = process.env.REACT_APP_DB_URL;
+
+export async function getProducts() {
+  try {
+    const response = await fetch(`${BASE_URL}/api/products`);
+    return response.json();
+  } catch (error) {
+    return new Error(error);
+  }
+}
+
+export async function getProduct(_id) {
+  try {
+    const response = await fetch(`${BASE_URL}/api/products/${_id}`);
+    return response.json();
+  } catch (error) {
+    return new Error(error);
+  }
+}
+
+/* export async function getClientProducts(host) {
+  const token = localStorage.getItem('token');
+  try {
+    const options = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        authorization: `Bearer ${token}`,
+      },
+    };
+    const response = await fetch(`${BASE_URL}/api/products/byClientId/${host}`, options);
+    return response.json();
+  } catch (error) {
+    return new Error(error);
+  }
+} */
+
+export async function createProduct(ProductRegister) {
+  const token = localStorage.getItem('token');
+  try {
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(ProductRegister),
+    };
+    const response = await fetch(`${BASE_URL}/api/products/`, options);
+    return response.json();
+  } catch (error) {
+    return new Error(error);
+  }
+}
+
+export async function deleteProduct(_id) {
+  const token = localStorage.getItem('token');
+  try {
+    const options = {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        authorization: `Bearer ${token}`,
+      },
+    };
+    const response = await fetch(`${BASE_URL}/api/products/${_id}`, options);
+    return response.json({ response, message: 'Product deleted' });
+  } catch (error) {
+    return new Error(error);
+  }
+}
