166a08673f43d703aced03f86e737ed949d4ffc1
adrianvilla1504 <adriancvilla@gmail.com>
2022-09-26 15:15:28 -0500

    feat: add cart products and admin register styles

---
+++ components/Cart/CartProducts.jsx
@@ -0,0 +1,76 @@
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { adjustItemQty } from "../../store/action/shopping";
+const CartProducts = ({ product, handleRemoveItem  }) => {
+
+  const dispatch = useDispatch();
+  const [input, setInput] = useState(product.qty);
+  const onChangeHandler = (e) => {
+    setInput(e.target.value);
+    dispatch(adjustItemQty( product._id, e.target.value));
+  };
+
+  return (
+    <div>
+      {
+        product ?
+        (<li key={product._id} className="flex py-6">
+        <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+          <img
+            src={product.img}
+            alt={product.name}
+            className="h-full w-full object-cover object-center"
+          />
+        </div>
+
+        <div className="ml-4 flex flex-1 flex-col">
+          <div>
+            <div className="flex justify-between text-base font-medium text-gray-900">
+              <h3>
+                <a href={product.name}>{product.name}</a>
+              </h3>
+              <p className="ml-4">{product.price}</p>
+            </div>
+          </div>
+          <div className="flex flex-1 items-end justify-between text-sm">
+            <input
+              className="h-[30px] border-black-500"
+              min="1"
+              key={product._id}
+              type="number"
+              id="qty"
+              name="qty"
+              value={input}
+              onChange={onChangeHandler}
+            />
+
+            <div className="flex">
+              <button
+                type="button"
+                className="font-medium text-[#FF9E00] hover:text-[#E18B00]"
+                onClick={() => handleRemoveItem(product._id)}
+              >
+                Remove
+              </button>
+            </div>
+          </div>
+        </div>
+      </li>
+      )
+      :
+      (
+        <>
+          <p>
+            ...wait
+          </p>
+        </>
+      )
+
+      }
+    </div>
+  );
+};
+
+export default CartProducts;
