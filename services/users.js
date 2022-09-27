2720b52d00fac8238ea59d45b6a160762f707fab
adrianvilla1504 <adriancvilla@gmail.com>
2022-09-26 21:51:53 -0500

    feat: env config, and back url

--- services/users.js
+++ services/users.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-underscore-dangle */
-const BASE_URL = process.env.REACT_APP_DB_URL;
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 export async function getUsers() {
   try {
