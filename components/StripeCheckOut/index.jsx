9348b888037d0587629fa3a353b4c90af46a33fa
adrianvilla1504 <adriancvilla@gmail.com>
2022-10-15 07:23:14 -0500

    feat: syntax error on .env

--- components/StripeCheckOut/index.jsx
+++ components/StripeCheckOut/index.jsx
@@ -66,7 +66,7 @@ export default function CheckoutForm() {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: `${NEXT_PUBLIC_DEPLOY_URL}/succespage`,
+        return_url: `${process.env.NEXT_PUBLIC_DEPLOY_URL}/succespage`,
       },
     });
 
